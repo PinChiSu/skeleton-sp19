2c442a6bf871bf088e485002a5ebefd712f1c481
PinChi <sueric68@gmail.com>
2020-04-22 23:52:54 +0800

    做到calcNetForceExertedByX and calcNetForceExertedByY,都有過unit test

--- proj0/Body.java
+++ proj0/Body.java
@@ -24,4 +24,24 @@ public class Body{
         mass = b.mass;
         imgFileName = b.imgFileName;
 	}
+
+	public double calcDistance(Body b){
+		return Math.sqrt(Math.pow(this.xxPos - b.xxPos,2) + Math.pow(this.yyPos - b.yyPos,2));
+	}
+
+	public double calcForceExertedBy(Body b){
+		return (this.mass * b.mass * (6.67e-11)) / Math.pow(this.calcDistance(b),2);
+	}
+
+	public double calcForceExertedByX(Body b){
+		return (this.calcForceExertedBy(b) * (b.xxPos - this.xxPos)) / this.calcDistance(b);
+	}
+
+	public double calcForceExertedByY(Body b){
+		return (this.calcForceExertedBy(b) * (b.yyPos - this.yyPos)) / this.calcDistance(b);
+	}
+
+	public double calcNetForceExertedByX(Body[] allBodys){
+		
+	}
 }
